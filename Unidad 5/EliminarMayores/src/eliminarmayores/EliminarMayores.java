de9abd31dcfdb16b7a44671cc9b2439d1c4e8e33
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-11-23 22:37:14 +0000

    Add func eliminarMayores

--- Unidad 5/EliminarMayores/src/eliminarmayores/EliminarMayores.java
+++ Unidad 5/EliminarMayores/src/eliminarmayores/EliminarMayores.java
@@ -1,9 +1,24 @@
 package eliminarmayores;
 
-public class EliminarMayores {
+import java.util.Arrays;
 
+public class EliminarMayores {
+    static int[] eliminarMayores(int t[], int valor) {
+        int copia[] = new int[0];
+        
+        for (int i : t) {
+            if (i <= valor) {
+                copia = Arrays.copyOf(copia, copia.length + 1);
+                copia[copia.length - 1] = i;
+            }
+        }
+        
+        return copia;
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int t[] = {4, 8, 9, 6, 11, -3, -4, 14, 5};
+        System.out.println(Arrays.toString(eliminarMayores(t, 8)));
     }
     
 }
