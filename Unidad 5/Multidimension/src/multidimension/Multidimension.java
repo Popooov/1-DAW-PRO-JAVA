573313e348d3854acd75de5ba583242d3829c1b7
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-07 23:58:14 +0000

    Initial commit

---
+++ Unidad 5/Multidimension/src/multidimension/Multidimension.java
@@ -0,0 +1,13 @@
+package multidimension;
+
+
+public class Multidimension {
+
+
+    public static void main(String[] args) {
+        // Crear una tabla bidimensional de longitud 5x5 y rellenarla de la siguiente forma:
+        // el elemento de la posición [n][m] debe contener el valor 10 x n + m.
+        // Después se debe mostrar su contenido.
+    }
+    
+}
