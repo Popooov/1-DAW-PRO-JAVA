13a245658552eb89e1748f3fe40e7a46d58295b6
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-13 23:19:53 +0000

    First commit

---
+++ Unidad 5/Ayuntamiento/src/ayuntamiento/Ayuntamiento.java
@@ -0,0 +1,18 @@
+package ayuntamiento;
+
+public class Ayuntamiento {
+
+    public static void main(String[] args) {
+        // El ayuntamiento de tu localidad te ha encargado una aplicación
+        // que ayude a realizar encuestas estadísticas 
+        // para conocer el nivel adquisitivo de los habitantes del municipio.
+        // Para ello, tendrás que preguntar el sueldo a cada persona encuestada.
+        // A príorí, no conoces el número de encuestados.
+        // Para finalizar la entrada de datos, introduce unsueldo con valor-1.
+        // Una vez terminada la entrada de datos, muestra la siguiente información:
+        // - Todos los sueldos introducidos ordenados de forma decreciente.
+        // - El sueldo máximo y mínimo.
+        // - La media de los sueldos.
+    }
+    
+}
