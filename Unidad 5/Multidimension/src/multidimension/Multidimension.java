30ed3b7ace035004aab4c7de5f8fd3985bafb1ca
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-13 23:08:45 +0000

    Exercise solution

--- Unidad 5/Multidimension/src/multidimension/Multidimension.java
+++ Unidad 5/Multidimension/src/multidimension/Multidimension.java
@@ -1,5 +1,7 @@
 package multidimension;
 
+import java.util.Arrays;
+
 
 public class Multidimension {
 
@@ -8,6 +10,17 @@ public class Multidimension {
         // Crear una tabla bidimensional de longitud 5x5 y rellenarla de la siguiente forma:
         // el elemento de la posición [n][m] debe contener el valor 10 x n + m.
         // Después se debe mostrar su contenido.
+        int bidimensional[][] = new int[5][5];
+        
+        for (int n = 0; n < bidimensional.length; n++) {
+            for (int m = 0; m < bidimensional[n].length; m++) {
+                bidimensional[n][m] = 10 * n + m;
+            }
+        }
+        
+        for (int[] element : bidimensional) {
+            System.out.println(Arrays.toString(element));
+        }
     }
     
 }
