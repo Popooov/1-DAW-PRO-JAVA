cf497cdfe6dc72ba5f4f78d1bf1ba5ae5694c393
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-18 22:43:01 +0000

    First commit

---
+++ Unidad 6/Invertida/src/invertida/Invertida.java
@@ -0,0 +1,10 @@
+package invertida;
+
+public class Invertida {
+
+    public static void main(String[] args) {
+        // Diseñar una función a la que se le pase una cadena de caracteres y la devuelva invertida.
+        // Un ejemplo, la cadena <hola mundo> quedaría <odnum aloH>.
+    }
+    
+}
