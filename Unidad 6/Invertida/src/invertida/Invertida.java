e9418af48703b0b26c216b80c3ce404b27313741
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-18 23:12:49 +0000

    Create a function to reverse a string

--- Unidad 6/Invertida/src/invertida/Invertida.java
+++ Unidad 6/Invertida/src/invertida/Invertida.java
@@ -1,10 +1,24 @@
 package invertida;
 
-public class Invertida {
+import java.util.Scanner;
 
+public class Invertida {
+    static void invertirCadena(String cadena) {
+        StringBuilder cadenaInvertida = new StringBuilder()
+                .append(cadena)
+                .reverse();
+        
+        System.out.println("Cadena invertida: " + cadenaInvertida);
+    }
+    
     public static void main(String[] args) {
         // Diseñar una función a la que se le pase una cadena de caracteres y la devuelva invertida.
         // Un ejemplo, la cadena <hola mundo> quedaría <odnum aloH>.
+        Scanner sc = new Scanner(System.in, "windows-1252");
+        System.out.print("Dame la cadena para invertir: ");
+        String cadena = sc.nextLine();
+        
+        invertirCadena(cadena);
     }
     
 }
