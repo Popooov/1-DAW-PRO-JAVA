d31a8ba341e9fced568ed6e26d97bcc515798a3d
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-19 12:07:08 +0000

    First commit

---
+++ Unidad 6/NumeroDePalabrasEnLaFrase/src/numerodepalabrasenlafrase/NumeroDePalabrasEnLaFrase.java
@@ -0,0 +1,10 @@
+package numerodepalabrasenlafrase;
+
+public class NumeroDePalabrasEnLaFrase {
+
+    public static void main(String[] args) {
+        // Diseñar un programa que solicite al usuario una frase y una palabra.
+        // A continuación buscará cuántas veces aparece la palabra en la frase.
+    }
+    
+}
