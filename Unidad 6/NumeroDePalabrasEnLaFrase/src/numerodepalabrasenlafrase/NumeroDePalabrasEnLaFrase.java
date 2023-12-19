d50c9e6b5f98365ab2b5094972e8c096cfbb0a24
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-19 12:27:36 +0000

    Solution

--- Unidad 6/NumeroDePalabrasEnLaFrase/src/numerodepalabrasenlafrase/NumeroDePalabrasEnLaFrase.java
+++ Unidad 6/NumeroDePalabrasEnLaFrase/src/numerodepalabrasenlafrase/NumeroDePalabrasEnLaFrase.java
@@ -1,10 +1,36 @@
 package numerodepalabrasenlafrase;
 
+import java.util.Scanner;
+
 public class NumeroDePalabrasEnLaFrase {
+    static int buscarPalabras(String frase, String palabra) {
+        String palabras[] = frase.split(" ");
+        int contador = 0;
+        
+        for (String p : palabras) {
+            if (p.equalsIgnoreCase(palabra)) {
+                contador++;
+            }
+        }
+        
+        return contador;
+    }
 
     public static void main(String[] args) {
         // Diseñar un programa que solicite al usuario una frase y una palabra.
         // A continuación buscará cuántas veces aparece la palabra en la frase.
+        String frase, palabra;
+        int cantidadPalabrasEncontradas;
+        
+        Scanner sc = new Scanner(System.in, "windows-1252");
+        System.out.print("Escribe una frase: ");
+        frase = sc.nextLine();
+        System.out.print("Escribe una palabra: ");
+        palabra = sc.nextLine();
+        
+        cantidadPalabrasEncontradas = buscarPalabras(frase, palabra);
+        
+        System.out.println("La palabra " + palabra + " aparece "  + cantidadPalabrasEncontradas + " veces en la frase.");
     }
     
 }
