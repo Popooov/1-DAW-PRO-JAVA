a8e32bdc980da88c612043155d8747d8aefd7245
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-22 23:35:48 +0000

    First commit

---
+++ Unidad 6/Codificacion/src/codificacion/Codificacion.java
@@ -0,0 +1,17 @@
+package codificacion;
+
+public class Codificacion {
+
+    public static void main(String[] args) {
+        // Se dispone de la siguiente relación de letras:
+        // Conjunto 1: |e|i|k|m|p|q|r|s|t|u|v|
+        // Conjunto 2: |p|v|i|u|m|t|e|r|k|q|s|
+        // Con ella es posible codificar un texto, convirtiendo cada letra del conjunto 1 en su correspondiente del conjunto 2.
+        // El resto de las letras no se modifican. 
+        // Los conjuntos se utilizan tanto para codificar mayúsculas como minúsculas, mostrando siempre la codificación en minúsculas.
+        // Un ejemplo: la palabra «PaquiTo» se codifica como «matqvko».
+        // Realizar un programa que codifique un texto. Para ello implementar la siguiente función:
+        // char codifica(char conjunto1[],char conjunto2[], char c) que devuelve el carácter c codificado según los conjuntos 1 y 2 que se le pasan.
+    }
+    
+}
