454077881173181d3118a5d6082bc07f0bde992f
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2023-12-22 23:33:31 +0000

    First commit

---
+++ Unidad 6/PalabraAPalabra/src/palabraapalabra/PalabraAPalabra.java
@@ -0,0 +1,12 @@
+package palabraapalabra;
+
+public class PalabraAPalabra {
+
+    public static void main(String[] args) {
+        // Introducir por teclado una frase palabra a palabra, 
+        // y mostrar la frase completa separando las palabras introducidas con espacios en blanco.
+        // Terminar de leer la frase cuando alguna de las palabras introducidas sea la cadena «fin»
+        // escrita con cualquier combinación de mayúsculas y minúsculas. La cadena «fin» no aparecerá en la frase final.
+    }
+    
+}
