4d6ff88417528c0cd97503b5a6db5d5f099253a9
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-01-12 19:14:22 +0000

    First commit

---
+++ Unidad 7/CuentaCorriente/src/cuentacorriente/CuentaCorriente.java
@@ -0,0 +1,14 @@
+package cuentacorriente;
+
+public class CuentaCorriente {
+
+    public static void main(String[] args) {
+    // Diseñar la clase CuentaCorriente, que almacena los datos: DNI y nombre del titular, así como el saldo. 
+    // Las operaciones típicas con una cuenta corriente son:
+    // · Crear una cuenta: se necesita el DNI y nombre del titular. El saldo inicial será 0.
+    // · Sacar dinero: el método debe indicar si ha sido posible llevar a cabo la operación, si existe saldo suficiente.
+    // · Ingresar dinero: se incrementa el saldo.
+    // · Mostrar información: muestra la información disponible de la cuenta corriente.
+    }
+    
+}
