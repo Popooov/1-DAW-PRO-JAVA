e2270b74efb38dbf0ffff522c0a8e8cbd52fccab
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-01-25 19:50:23 +0000

    First commit

---
+++ Unidad 7/Punto/src/punto/Main.java
@@ -0,0 +1,7 @@
+package punto;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Unidad 7/Punto/src/punto/Punto.java
@@ -0,0 +1,64 @@
+package punto;
+
+public class Punto {
+    
+    // Escribe la clase Punto que representa un punto en el plano (con un componente x y un componente y), con los métodos:
+
+        // - Punto(double x, double y): construye un objeto con los datos pasados como parámetros.
+
+        // - void desplazaX(double dx): incrementa el componente x en la cantidad dx.
+
+        // - void desplazaY(double dy): incrementa el componente y en la cantidad dy.
+
+        // - void desplaza(double dx, double dy): desplaza ambos componentes según las cantidades dx (en el eje x) y dy (en el componente y).
+
+        // - doble distanciaEuclidea(Punto otro): calcula y devuelve la distancia euclídea entre el punto invocante y el punto otro.
+
+        // - void muestra(): muestra por consola la información relativa al punto
+    
+    private double x;
+    private double y;
+    
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+    void desplazaX(double dx) {
+        this.x += dx;
+    }
+    
+    void desplazaY(double dy) {
+        this.y += dy;
+    }
+    
+    void desplaza(double dx, double dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+    
+    double distanciaEuclidea(Punto otro) {
+        return Math.cbrt(4); // terminar
+    }
+    
+    void muestra() {
+        System.out.println("x: " + this.x);
+        System.out.println("y: " + this.y);
+    }
+}
