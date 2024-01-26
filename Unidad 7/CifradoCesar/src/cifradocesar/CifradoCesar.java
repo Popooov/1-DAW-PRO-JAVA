f255a1e2a7b7e0ba7fb4c7972e1fe96f8371c063
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-01-26 18:20:52 +0000

    First commit

--- Unidad 7/CifradoCesar/src/cifradocesar/CifradoCesar.java
+++ Unidad 7/CifradoCesar/src/cifradocesar/CifradoCesar.java
@@ -2,4 +2,16 @@ package cifradocesar;
 
 public class CifradoCesar {
     
+    int codificador;
+    String texto;
+    String textoCodificado;
+    
+    public CifradoCesar(int codificador, String texto) {
+        this.codificador = codificador;
+        this.texto = texto;
+    }
+    
+    static void cifrar() {
+        
+    }
 }
