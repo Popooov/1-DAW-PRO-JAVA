d7839c08af0a71f83564a47cf3751d650be9a880
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-01-26 19:49:46 +0000

    First commit

---
+++ Unidad 7/Hora/src/hora/Hora.java
@@ -0,0 +1,23 @@
+package hora;
+
+public class Hora {
+    int hora;
+    int minuto;
+    
+    Hora(int hora, int minuto) {
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+    
+    void inc() {
+        this.minuto++;
+    }
+    
+    boolean setHora(int valor) {
+        return !(valor < 0 || valor > 23);
+    }
+    
+    boolean setMinutos(int valor) {
+        return !(valor < 0 || valor > 59);
+    }
+}
