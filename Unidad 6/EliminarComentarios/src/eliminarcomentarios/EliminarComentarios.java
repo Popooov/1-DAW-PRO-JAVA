437dfb113f582de54a651087505686f29df6603b
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-01-29 23:23:59 +0000

    Add for loop

--- Unidad 6/EliminarComentarios/src/eliminarcomentarios/EliminarComentarios.java
+++ Unidad 6/EliminarComentarios/src/eliminarcomentarios/EliminarComentarios.java
@@ -1,7 +1,10 @@
 package eliminarcomentarios;
 
-
 public class EliminarComentarios {
+    
+//    public String eliminarComentarios (String sentencia) {
+//        
+//    }
 
     public static void main(String[] args) {
         // El preprocesador del lenguaje C elimina los comentarios (/*... */) del código fuente antes de compilar. 
@@ -10,6 +13,22 @@ public class EliminarComentarios {
         // if (a==3) /* igual a tres */
         //      a++; /* incrementamos a */
         // Salida: if (a==3) a++;
+        int startOfComment = 0, endOfComment = 0;
+        String sentencia = """
+                            if (a==3) /* igual a tres */
+                                a++; /* incrementamos a */""";
+        
+        for (int i = 0; i < sentencia.length(); i++) {
+            System.out.print(sentencia.charAt(i));
+            if (sentencia.charAt(i) == '/') {
+                startOfComment = sentencia.indexOf('/');
+            }
+        }
+        
+        System.out.println(startOfComment);
+        System.out.println(endOfComment);
+//        System.out.println(sentencia.indexOf('/'));
+//        System.out.println(sentencia.length());
     }
     
 }
