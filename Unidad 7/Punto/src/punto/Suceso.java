35eee69c20fe4b6a235fe21aafa0b8ddbbf42cfd
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-08 13:53:30 +0000

    Add Suceso subclass

---
+++ Unidad 7/Punto/src/punto/Suceso.java
@@ -0,0 +1,11 @@
+package punto;
+
+public class Suceso extends Punto3D {
+    int tiempo;
+    String descripcion;
+
+    public Suceso(double x, double y, double z) {
+        super(x, y, z);
+    }
+    
+}
