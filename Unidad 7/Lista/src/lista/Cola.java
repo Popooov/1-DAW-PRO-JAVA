0265ea171cfcbbee8b22561c9be90995c0173471
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-16 17:56:28 +0000

    Add Cola and Pila interfaces

---
+++ Unidad 7/Lista/src/lista/Cola.java
@@ -0,0 +1,6 @@
+package lista;
+
+public interface Cola {
+    void encolar(Integer n);
+    Integer desencolar();
+}
---
+++ Unidad 7/Lista/src/lista/Pila.java
@@ -0,0 +1,6 @@
+package lista;
+
+public interface Pila {
+    void apilar(Integer n);
+    Integer desapilar();
+}
