1ae876b1d64a17b5915db591701ee647bc7e0555
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-19 22:55:43 +0000

    Finish the task

--- Unidad 6/PalabraAPalabra/src/palabraapalabra/PalabraAPalabra.java
+++ Unidad 6/PalabraAPalabra/src/palabraapalabra/PalabraAPalabra.java
@@ -1,5 +1,7 @@
 package palabraapalabra;
 
+import java.util.Scanner;
+
 public class PalabraAPalabra {
 
     public static void main(String[] args) {
@@ -7,6 +9,18 @@ public class PalabraAPalabra {
         // y mostrar la frase completa separando las palabras introducidas con espacios en blanco.
         // Terminar de leer la frase cuando alguna de las palabras introducidas sea la cadena «fin»
         // escrita con cualquier combinación de mayúsculas y minúsculas. La cadena «fin» no aparecerá en la frase final.
+        String palabra,
+                fin = "fin",
+                frase = "";
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Introduce una palabra");
+            palabra = sc.nextLine();
+            frase += palabra + " ";
+        } while (!palabra.toLowerCase().contains(fin));
+        
+        System.out.println(frase.toLowerCase().replace("fin", ""));
     }
-    
+
 }
