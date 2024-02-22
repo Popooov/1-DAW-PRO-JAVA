3298d6944c392bd7add5f06fdc5ed4103b04e390
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-22 23:35:37 +0000

    Add attributes and constructor

--- Unidad 7/Calendario/src/calendario/Calendario.java
+++ Unidad 7/Calendario/src/calendario/Calendario.java
@@ -2,4 +2,15 @@ package calendario;
 
 public class Calendario {
 
+    int anyo, mes, dia;
+
+    public Calendario(int anyo, int mes, int dia) {
+        if (anyo >= 0 && anyo <= 2024) {
+            this.anyo = anyo;
+        } else if (mes >= 1 && mes <= 12) {
+            this.mes = mes;
+        } else if (dia >= 1 && dia <= 31) {
+            this.dia = dia;
+        }
+    }
 }
