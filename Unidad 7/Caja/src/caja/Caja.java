24b7305e67c91a5bbfdced4dbf355a0a4ea8b072
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-23 16:06:52 +0000

    Add enum Unidad, constructor

--- Unidad 7/Caja/src/caja/Caja.java
+++ Unidad 7/Caja/src/caja/Caja.java
@@ -1,5 +1,13 @@
 package caja;
 
 public class Caja {
-
+    final int ancho, alto, fondo;
+    private String etiqueta;
+    Unidad unidadMedida;
+    
+    public Caja(int ancho, int alto, int fondo) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.fondo = fondo;
+    }
 }
