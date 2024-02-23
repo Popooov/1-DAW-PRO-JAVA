2debd2367e9d8cb76a315fc4160554f2c031fce1
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-02-23 17:04:51 +0000

    Finish the Caja class

--- Unidad 7/Caja/src/caja/Caja.java
+++ Unidad 7/Caja/src/caja/Caja.java
@@ -5,7 +5,7 @@ public class Caja {
     final int ancho, alto, fondo;
     private String etiqueta;
     Unidad unidadMedida;
-    private double volumen;
+    private final double volumen;
 
     public Caja(int ancho, int alto, int fondo, Unidad unidadMedida) {
         this.ancho = ancho;
@@ -13,18 +13,40 @@ public class Caja {
         this.fondo = fondo;
         this.unidadMedida = unidadMedida;
         if (this.unidadMedida == Unidad.CM) {
-            volumen = (this.ancho / 100) * (this.alto / 100) * (this.fondo / 100);
+            volumen = (double) (ancho * alto * fondo) / 100;
         } else {
-            volumen = this.ancho * this.alto * this.fondo;
+            volumen =  ancho * alto * fondo;
         }
     }
     
     public double getVolumen() {
         return volumen;
     }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
     
     @Override
     public String toString() {
-        return "hola";
+        if (unidadMedida == Unidad.CM) {
+            return "El destinatario: " + this.etiqueta + "." +
+                    "\nLas medidas de la caja son: " + 
+                    "\nel ancho: " + this.ancho + "cm, " +
+                    "\nel alto: " + this.alto + "cm, " +
+                    "\nel fondo: " + this.fondo + "cm, " +
+                    "\nel volumen: " + this.volumen + " cm.";
+        } else {
+            return "El destinatario: " + this.etiqueta + "." +
+                    "\nLas medidas de la caja son: " + 
+                    "\nel ancho: " + this.ancho + "m, " +
+                    "\nel alto: " + this.alto + "m, " +
+                    "\nel fondo: " + this.fondo + "m, " +
+                    "\nel volumen: " + this.volumen + "m.";
+        }
     }
 }
--- Unidad 7/Caja/src/caja/Main.java
+++ Unidad 7/Caja/src/caja/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         Caja c1 = new Caja(6, 5, 2, Unidad.CM);
-        System.out.println(c1.getVolumen());
-        
+        System.out.println("El volumen de la caja: " + c1.getVolumen() + "cm");
+        c1.setEtiqueta("Olek Popov, Breña Alta");
+        System.out.println("Destinatario: " + c1.getEtiqueta());
+        System.out.println("Info de la caja:");
+        System.out.println(c1.toString());
+        System.out.println("--------------------------------------");
+        Caja c2 = new Caja(6, 5, 2, Unidad.M);
+        System.out.println("El volumen de la caja: " + c2.getVolumen() + "m");
+        c2.setEtiqueta("Olek Popov, Breña Alta");
+        System.out.println("Destinatario: " + c2.getEtiqueta());
+        System.out.println("Info de la caja:");
+        System.out.println(c2.toString());
     }
 }
