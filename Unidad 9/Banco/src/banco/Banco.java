5ea9043169fbe174eb8da1cde1be9068c461ee7d
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-03-08 18:18:59 +0000

    Add classes to the main

--- Unidad 9/Banco/src/banco/Banco.java
+++ Unidad 9/Banco/src/banco/Banco.java
@@ -3,8 +3,24 @@ package banco;
 public class Banco {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        //new blabla(asdfdsf, sdafsd, true, false)
+        Persona p1 = new Persona("00000000A", "Olek", true, true);
+        Cliente c1 = p1;
+        c1.ingresarSaldo(100.0);
+        
+        Empleado e1 = p1;
+        e1.setHorasTrabajadas(20);
+        
+        System.out.println(e1);
+        
+        Persona p2 = new Persona("00000000A", "Olek", false, true);
+        Cliente c2 = p2;
+        c2.ingresarSaldo(100.0);
+        System.out.println(c2);
+        
+        Persona p3 = new Persona("00000000A", "Olek", true, false);
+        Empleado e2 = p3;
+        e2.setHorasTrabajadas(10);
+        System.out.println(e2);
     }
 
 }
