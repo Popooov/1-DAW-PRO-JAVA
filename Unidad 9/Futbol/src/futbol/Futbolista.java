62d73a50d4b5517828b953326baa47e02284136d
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-03-15 14:05:35 +0000

    Add Futbolista class

---
+++ Unidad 9/Futbol/src/futbol/Futbolista.java
@@ -0,0 +1,66 @@
+package futbol;
+
+import java.util.Objects;
+
+public class Futbolista {
+
+    private String dni, nombre;
+    private int edad, numeroGoles;
+
+    public Futbolista(String dni, String nombre, int edad, int numeroGoles) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numeroGoles = numeroGoles;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumeroGoles() {
+        return numeroGoles;
+    }
+
+    public void setNumeroGoles(int numeroGoles) {
+        this.numeroGoles = numeroGoles;
+    }
+    
+    @Override
+    public String toString() {
+        return "Futbolista{" + "dni: " + dni + ", nombre: " + nombre + ", edad: " + edad + ", numeroGoles: " + numeroGoles + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean iguales = false;
+        final Futbolista otro = (Futbolista) obj;
+        
+        if (this.dni.equals(otro.dni)) {
+            iguales = true;
+        }
+        
+        return iguales;
+    }
+    
+}
