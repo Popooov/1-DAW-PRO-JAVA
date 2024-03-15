698b05f34683185164509840fd30a5bf28a83d21
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2024-03-15 11:55:25 +0000

    Add Detergente class

---
+++ Unidad 9/Supermercado/src/supermercado/Detergente.java
@@ -0,0 +1,61 @@
+package supermercado;
+
+public class Detergente implements EsLiquido, ConDescuento {
+
+    String marca, tipoEnvase;
+    double precio,
+            volumen,
+            descuento;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public void setVolumen(double v) {
+        this.volumen = v;
+    }
+
+    @Override
+    public double getVolumen() {
+        return this.volumen;
+    }
+
+    @Override
+    public void setTipoEnvase(String env) {
+        this.tipoEnvase = env;
+    }
+
+    @Override
+    public String getTipoEnvase() {
+        return this.tipoEnvase;
+    }
+
+    @Override
+    public void setDescuento(double des) {
+        this.descuento = des;
+    }
+
+    @Override
+    public double getDescuento() {
+        return this.descuento;
+    }
+
+    @Override
+    public double getPrecioDescuento() {
+        return this.precio - (this.precio * this.descuento);
+    }
+
+}
